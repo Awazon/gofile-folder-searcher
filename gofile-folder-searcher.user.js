404d189c8a62db54e60f749ff250c7a3187cf238
Awazon <61077801+Awazon@users.noreply.github.com>
2024-12-17 13:28:22 +0900

    guiのidを変更 関数名を変更

--- gofile-folder-searcher.user.js
+++ gofile-folder-searcher.user.js
@@ -9,59 +9,62 @@
 // ==/UserScript==
 
 (() => {
-    const gui = {
-        buttons: `
-        <div id="filemanager_extensionbuttons" class="flex items-center justify-end">
+	const gui = {
+		buttons: `
+        <div id="filemanager_extension" class="flex items-center justify-end">
             <div class="flex items-center space-x-2 relative">
-                <button id="filemanager_extensionbuttons_search" class="bg-gray-600 text-white px-2 py-1 rounded shadow hover:bg-gray-700 flex items-center text-sm">
+                <button id="filemanager_extension_search" class="bg-gray-600 text-white px-2 py-1 rounded shadow hover:bg-gray-700 flex items-center text-sm">
                     <i class="fa fa-search-plus mr-1"></i>
                     <span class="hidden lg:inline">Search</span>
                 </button>
-                <button id="filemanager_extensionbuttons_fetch" class="bg-gray-600 text-white px-2 py-1 rounded shadow hover:bg-gray-700 flex items-center text-sm">
+                <button id="filemanager_extension_fetch" class="bg-gray-600 text-white px-2 py-1 rounded shadow hover:bg-gray-700 flex items-center text-sm">
                     <i class="fas fa-file-import mr-1"></i>
                     <span class="hidden lg:inline">Fetch folder content</span>
                 </button>
             </div>
         </div>
         `,
-    }
+	};
 
-    const configs = {
+	const configs = {};
+	const utils = {
+		log(message) {
+			console.log(`[EXT] ${message}`);
+		},
+	};
 
-    }
-    const utils = {
-        log(message) {
-            console.log(`[EXT] ${message}`);
-        }
-    }
+	const init = {
+		addGui() {
+			//add button
+			const mainButtonNode = document.getElementById(
+				"filemanager_mainbuttons"
+			);
+			mainButtonNode.insertAdjacentHTML("afterend", gui.buttons);
 
-    const init = {
-        addButtons() {
-            const mainButtonNode = document.getElementById('filemanager_mainbuttons');
-            mainButtonNode.insertAdjacentHTML('afterend', gui.buttons)
-        },
-        setObserver() {
-            const fileListNode = document.getElementById('index_main');
+			//add
+		},
+		setObserver() {
+			const fileListNode = document.getElementById("index_main");
 
-            if (fileListNode) {
-                const observer = new MutationObserver((mutationsList) => {
-                    for (const mutation of mutationsList) {
-                        if (mutation.type === 'childList') {
-                            mutation.addedNodes.forEach((node) => {
-                                if (node.id === 'filemanager') {
-                                    init.addButtons()
-                                }
-                            });
-                        }
-                    }
-                });
+			if (fileListNode) {
+				const observer = new MutationObserver((mutationsList) => {
+					for (const mutation of mutationsList) {
+						if (mutation.type === "childList") {
+							mutation.addedNodes.forEach((node) => {
+								if (node.id === "filemanager") {
+									init.addGui();
+								}
+							});
+						}
+					}
+				});
 
-                const config = { childList: true, subtree: false };
-                observer.observe(fileListNode, config);
-            } else {
-                utils.log('index_main not found');
-            }
-        },
-    }
-    init.setObserver()
+				const config = {childList: true, subtree: false};
+				observer.observe(fileListNode, config);
+			} else {
+				utils.log("index_main not found");
+			}
+		},
+	};
+	init.setObserver();
 })();
